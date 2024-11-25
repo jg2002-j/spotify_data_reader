8618d828cfdf1673a746e95b1a68be85f18735e9
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-11-25 00:38:40 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Button, Checkbox, CheckIcon } from "@headlessui/react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [enabled, setEnabled] = useState(true);
+
+  return (
+    <div className="">
+      <h1>Spotify JSON Reader</h1>
+      <div className="bg-black">
+        <Button
+          className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is {count}
+        </Button>
+        <Checkbox
+          checked={enabled}
+          onChange={setEnabled}
+          className="group size-6 rounded-md bg-white/10 p-1 ring-1 ring-white/15 ring-inset data-[checked]:bg-white"
+        >
+          <CheckIcon className="hidden size-4 fill-black group-data-[checked]:block" />
+        </Checkbox>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    // colors: {
+    //   spotify_green: "1ED760",
+    //   spotify_black: "121212",
+    // },
+    fontFamily: {
+      sans: ["Inter"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
